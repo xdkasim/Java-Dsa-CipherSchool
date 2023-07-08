a06709c400ff677ebd5a7c43d5b877e6aa15b2f4
Asim <iqbalasim929@gmail.com>
2023-07-08 18:52:46 +0530

    version 1

---
+++ lecture-4/prmdt.java
@@ -0,0 +1,13 @@
+public class prmdt {
+    public static void main(String[] args ){
+        int x=5;
+        int y=7;
+        System.out.println("X is: " + x);
+        System.out.println("Y is: " + y);
+        byte b=10;
+        System.out.println("B is: " +b);
+
+        int i=b;
+        System.out.println("I is: " + i);
+    }
+}
