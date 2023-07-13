c6882fcc6fee08af15c7f12e7bc5b345756700f1
asim <iqbalasim929@gmail.com>
2023-07-13 12:19:42 +0530

    if else

---
+++ Lecture7-CipherSchool/CarStudy.java
@@ -0,0 +1,30 @@
+class Car{
+    String name;
+    String color;
+    int year;
+    int maxSpeed;
+    void accelerate(){
+        System.out.println("Car is accelerating");
+    }
+}
+public class CarStudy{
+    public static void main(String[] args){
+        Car polo =new Car();
+        System.out.println(polo.name);
+        System.out.println(polo.color);
+        System.out.println(polo.year);
+        System.out.println(polo.maxSpeed);
+
+        polo.name = "polo";
+        polo.color = "Red";
+        polo.year = 2016;
+        polo.maxSpeed = 120;
+
+        System.out.println(polo.name);
+        System.out.println(polo.color);
+        System.out.println(polo.year);
+        System.out.println(polo.maxSpeed);
+
+    }
+}
+
